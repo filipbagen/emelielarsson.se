3ba5f15b2a65c83a9c722243338379762286d48d
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-01-15 20:51:17 +0100

    Hero section finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
+// components
+import ProfileCard from '../src/components/ProfileCard.tsx';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="flex flex-col justify-center items-center bg-primary w-screen h-screen">
+      {/* <div className="absolute top-0 right-0 w-1/2 h-1/2 bg-white rounded-bl-lg shadow-inner inset-shadow"> */}
+      {/* Content */}
+      {/* </div> */}
+      <ProfileCard />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#E4DACF',
+      secondary: '#D3AAAA',
+      white: '#ffffff',
+      black: '#343434',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Wix Madefor Text"', 'sans-serif'],
+        body: ['"Lato"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
